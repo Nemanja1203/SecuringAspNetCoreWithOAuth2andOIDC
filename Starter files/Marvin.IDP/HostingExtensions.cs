5a097c1a87c55c97998cd3e85daad9216a520709
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2024-11-10 23:16:01 +0100

    Enabling razor pages (IPD UI)

--- Starter files/Marvin.IDP/HostingExtensions.cs
+++ Starter files/Marvin.IDP/HostingExtensions.cs
@@ -7,7 +7,7 @@ namespace Marvin.IDP
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
             // uncomment if you want to add a UI
-            //builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
             builder.Services.AddIdentityServer(options =>
                 {
@@ -31,14 +31,14 @@ namespace Marvin.IDP
             }
 
             // uncomment if you want to add a UI
-            //app.UseStaticFiles();
-            //app.UseRouting();
+            app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseIdentityServer();
 
             // uncomment if you want to add a UI
-            //app.UseAuthorization();
-            //app.MapRazorPages().RequireAuthorization();
+            app.UseAuthorization();
+            app.MapRazorPages().RequireAuthorization();
 
             return app;
         }
