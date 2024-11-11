a7d92fba7cdafe2ea741e7b360e953c8a8588f9f
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2024-11-11 23:18:56 +0100

    Adding OpenIdConnect handling on client side

--- Starter files/ImageGallery.Client/Program.cs
+++ Starter files/ImageGallery.Client/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-    .AddJsonOptions(configure => 
+    .AddJsonOptions(configure =>
         configure.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 // create an HttpClient used for accessing the API
@@ -15,7 +17,30 @@ builder.Services.AddHttpClient("APIClient", client =>
     client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
 });
 
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = "https://localhost:5001/"; // Address of our Identity Provider
+        options.ClientId = "imagegalleryclient"; // Should match cliend id on the level of Identity Provider
+        options.ClientSecret = "secret"; // Should match secret we configured on the level of Identity Provider
+        options.ResponseType = "code";
+        //options.Scope.Add("openid"); // By default openid and profile scopes are requested by the middleware
+        //options.Scope.Add("profile");
+        //options.CallbackPath = new PathString("signin-oidc");
+        options.SaveTokens = true;
+    });
+
+ 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -29,6 +54,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
