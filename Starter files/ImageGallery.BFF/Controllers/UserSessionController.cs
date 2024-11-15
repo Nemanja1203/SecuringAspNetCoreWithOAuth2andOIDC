e3ea2e1b244b2b037d77b1fb76c2836261e24858
Nemanja Milosevic <nemanja.m.milosevic@gmail.com>
2024-11-15 18:05:13 +0100

    Getting access to user information

---
+++ Starter files/ImageGallery.BFF/Controllers/UserSessionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.BFF.Controllers
+{
+    [Authorize]
+    public class UserSessionController : Controller
+    {
+        public IActionResult GivenName()
+        {
+            var objectToReturn = new
+            {
+                given_name = User.Claims
+                    .FirstOrDefault(c => c.Type == "given_name")?.Value
+            };
+
+            return Json(objectToReturn);
+        }
+    }
+}
